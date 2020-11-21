20375c751b7f38558d1092e1c792b6a75272ca4c
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 13:02:50 -0300

    ejerciocio js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Porcentaje')
+
+alert('Porcentaje')
+
+const numero = prompt('Ingresa el número inicial');
+const porcentaje = prompt('Ingresá el porcentaje que querés calcular (sin el %)');
+const resultado = (numero * porcentaje)/100;
+
+alert(`El ${porcentaje}% de ${numero} es ${resultado}.`);
